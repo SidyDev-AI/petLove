274f75b76391729b4d7a48d6881c3ecc2ddb8fdd
Borges Costta <sidydev.io@gmail.com>
2025-03-03 12:10:18 -0300

    Iniciando programação do projeto

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="pt-br ">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+  <link rel="stylesheet" href="src/css/styles.css">
+  <title>PetLove | Home</title>
+</head>
+<body>
+  <header>
+    <img src="src/img/LogoBannerPNG.png" alt="Logo PetLove" class="logo">
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Services</li>
+        <li>Shop</li>
+        <li>About</li>
+      </ul>
+    </nav>
+    <input type="text" placeholder="Search">
+    <div class="links">
+      <a href="#">
+        <i class="fa-solid fa-user"></i>
+      </a>
+      <a href="#">
+        <i class="fa-solid fa-cart-plus"></i>
+      </a>
+    </div>
+  </header>
+  <main>
+    <section class="banner">
+      <img class="logo" src="src/img/LogoPequenaV2PNG.png" alt="Logo PetLove">
+      <div class="frase">
+        <h1>Onde o amor pelos <br> <span>Animais</span> <br> é nossa insperação!</h1>
+        <img src="src/img/patas_1.png">
+      </div>
+      <img class="ilustracao" src="src/img/imgLove.png">
+    </section>
+
+    <section id="services">
+      <h1>Our Services</h1>
+      <div class="services">
+        <div class="services-left">
+          <div class="left-1">
+            <div class="service">
+              <h2>Serviços Estéticos</h2>
+              <p>Alguns deles são: banhos, hidratação, tosa na máquina,
+              tosa na tesoura, tosa higiênica, desembaraçamento, 
+              tingimento dos pelos, escovação de dentes, 
+              limpeza de ouvido e corte de unhas.</p>
+            </div>
+            <img src="src/img/cat_2.png">
+            <a class="btn" href="#">Click Here</a>
+          </div>
+          <div class="left-2">
+            <div class="service">
+              <h2>Bem-Estar</h2>
+              <p>Sugerimos esses serviços: dog walker/passeador, prática de esportes guiada como 
+              natação e corridas, ioga, acupuntura e fitoterápicos (sessões de relaxamento).</p>
+            </div>
+            <img src="src/img/dop&people.png">
+            <a class="btn" href="#">Click Here</a>
+          </div>
+        </div>
+        <div class="img-center">
+          <img class="pata-2" src="src/img/patas_2.png">
+          <img class="dog" src="src/img/dog.png">
+          <img class="pata-1" src="src/img/patas_2.png">
+        </div>
+        <div class="services-right">
+          <div class="right-1">
+            <div class="service">
+              <h2>Serviços Médicos</h2>
+              <p>Podemos considerar: consultas clínicas gerais e especialidades, atendimento domiciliar, 
+              vacinação, preventivos e cirurgias.</p>
+            </div>
+            <img src="src/img/cat_1.png">
+            <a class="btn" href="#">Click Here</a>
+          </div>
+          <div class="right-2">
+            <div class="service">
+              <h2>Educação</h2>
+              <p>São eles: adestramento e hospedagem (aprendizagem de convívio com outros animais).</p>
+            </div>
+            <img src="src/img/dog_1.png">
+              <a class="btn" href="#">Click Here</a>
+            </div>
+          </div>
+      </div>
+    </section>
+
+    <section class="about">
+      <h1>About</h1>
+    </section>
+  </main>
+  <footer></footer>
+</body>
+</html>
